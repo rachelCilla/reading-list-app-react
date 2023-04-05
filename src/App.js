836bfbae3b8e665c9d7e11813ed70f6f147d744c
appaandbuki <104958634+rachelCilla@users.noreply.github.com>
2023-04-05 13:58:53 -0600

    Book Create Styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App() {
-  return <div className="App"></div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log(title);
+  };
+
+  return (
+    <div className="App">
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function BookCreate() {
-  return <div>BookCreate</div>;
+export default function BookCreate({ onCreate }) {
+  const [title, setTile] = useState("");
+
+  const handleChange = (e) => {
+    setTile(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onCreate(title);
+    setTile("");
+  };
+
+  return (
+    <div className="book-create">
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input
+          className="input"
+          type="text"
+          value={title}
+          onChange={handleChange}
+        />
+        <button className="button" type="submit">
+          Create Book
+        </button>
+      </form>
+    </div>
+  );
 }
